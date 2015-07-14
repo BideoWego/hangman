2cd713ea081797bd6131dd579fd019d4e12bf736
Chris Scavello <bideowego@gmail.com>
2015-07-14 00:57:04 -0400

    Add Facebook and Twitter open graph support

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+set :public_folder, Proc.new { File.join(File.dirname(__FILE__), 'public') }
+
 require_relative 'resources/art.rb'
 require_relative 'resources/words.rb'
 require_relative 'lib/game.rb'
@@ -7,10 +9,7 @@ require_relative 'lib/game.rb'
 get '/'  do
 	Game.words = $words
 	Game.state = params
-
-	if Game.playing
-		erb :game
-	else
-		erb :menu
-	end
+	locals = {:base_url => request.url}
+	view = (Game.playing) ? :game : :menu
+	erb view, :locals => locals
 end
